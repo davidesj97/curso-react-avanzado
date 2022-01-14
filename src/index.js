b27be5b9be29c285a5c686854f1cf30515e3607d
David Sánchez <davidesj97@gmail.com>
2022-01-14 11:33:45 -0600

    Instalación de "vercel" y configuración de "ESLint"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 ReactDOM.render(
   <h1>Hola Platzi!!!</h1>,
   document.getElementById('app')
-);
+)
